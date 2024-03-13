8118f175d78c0d89d179189812596733b85e554d
Radhe Krishna <21ss02it026@ppsu.ac.in>
2024-03-13 13:10:42 +0530

    Initial commit

---
+++ lib/emi.dart
@@ -0,0 +1,370 @@
+import 'package:flutter/material.dart';
+
+class Emi extends StatefulWidget {
+  const Emi({super.key});
+
+  @override
+  State<Emi> createState() => _EmiState();
+}
+
+class _EmiState extends State<Emi> {
+  TextEditingController txtAmount = TextEditingController();
+  TextEditingController txtInterest = TextEditingController();
+  TextEditingController txtTenure = TextEditingController();
+
+  double ans = 00;
+  double EMI = 00;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 40,
+        centerTitle: true,
+        title: const Text(
+          "EMI Calculator",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_circle_left_outlined,
+          ),
+          iconSize: 30,
+          onPressed: () {
+            Navigator.pushNamed(context, "/");
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text(
+                  "Loan Amount",
+                  style: TextStyle(color: Color(0xffC796C5), fontSize: 17),
+                ),
+                SizedBox(
+                  width: 22,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: txtAmount,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffC796C5), width: 2)),
+                      border: OutlineInputBorder(
+                          // borderSide:
+                          ),
+                      hintText: "Enter your amount",
+                      contentPadding: EdgeInsets.all(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text(
+                  "Interest (%)",
+                  style: TextStyle(color: Color(0xffC796C5), fontSize: 17),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 37,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: txtInterest,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffC796C5), width: 2)),
+                      border: OutlineInputBorder(
+                          // borderSide:
+                          ),
+                      hintText: "Enter your interest",
+                      contentPadding: EdgeInsets.all(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Tenure",
+                          style:
+                              TextStyle(color: Color(0xffC796C5), fontSize: 17),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Row(
+                      children: [
+                        Text  (
+                          "(Years/Months)",
+                          style:
+                              TextStyle(color: Color(0xffC796C5), fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+                width: 40,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 11),
+                  child: TextField(
+                    controller: txtTenure,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffC796C5), width: 2)),
+                      border: OutlineInputBorder(),
+                      hintText: "year",
+                      contentPadding: EdgeInsets.all(10)
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+                width: 20,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffC796C5), width: 2)),
+                      border: OutlineInputBorder(),
+                      hintText: "Months",
+                      contentPadding: EdgeInsets.all(10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      EMI = (int.parse(txtAmount.text) * int.parse(txtInterest.text) * int.parse(txtTenure.text))/100;
+                      ans = (int.parse(txtAmount.text) + EMI);
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:10,top: 10),
+                    child: Container(
+                        height: 40,
+                        width: 115,
+                        decoration: BoxDecoration(
+                          color: Color(0xffC796C5),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Calculate",
+                            style: TextStyle(color: Color(0xffFFFBFE), fontSize: 16),
+                          ),
+                        ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:10,top: 10),
+                    child: Container(
+                        height: 40,
+                        width: 115,
+                        decoration: BoxDecoration(
+                          color: Color(0xffC796C5),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Reset",
+                            style: TextStyle(color: Color(0xffFFFBFE), fontSize: 16),
+                          ),
+                        ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:10,top: 10,right: 20),
+                    child: Container(
+                        height: 40,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0xffC796C5),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "History",
+                            style: TextStyle(color: Color(0xffFFFBFE), fontSize: 16),
+                          ),
+                        ),
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Text(
+                  "EMI Calculation Result",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 15),
+                child: Text(
+                  "Monthly",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 280, top: 15),
+                child: Text(
+                  "00",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  "Total Interest",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 244.5, top: 20),
+                child: Text("$EMI",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  "Total Payment",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 238, top: 20),
+                child: Text("$ans",
+                  style: TextStyle(
+                      color: Color(0xff202121),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10,right: 20,top: 50),
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                      });
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Color(0xffC796C5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Check Details",
+                          style: TextStyle(color: Color(0xffFFFBFE), fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+              height: 250, width: 400, child: Image.asset("calculate.png")),
+          const Padding(
+            padding: EdgeInsets.only(top: 70),
+            child: Text(
+              "Emi Calculator",
+              style: TextStyle(
+                  color: Color(0xffC796C5),
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Text("Easy to calculator Installment",style: TextStyle(color: Colors.black),),
+          const Text("With static`s",style: TextStyle(color: Colors.black),),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top:50),
+                  child: Center(
+                    child: GestureDetector(child: Stack(
+                              children: [
+                                Positioned(
+                                child: Container(
+                                height: 150,
+                                width: 140,
+                                decoration: BoxDecoration(
+                      color: const Color(0xffE3D5E6),
+                      borderRadius: const BorderRadius.all(Radius.circular(10),),
+                      border: Border.all(color: const Color(0xffC796C5),width: 2)
+                      ),
+                    ),
+                                ),
+                    const Positioned(
+                    height: 100,
+                     width: 100,
+                        left: 20,
+                        child: Image(image: AssetImage("about.png"))
+                       ),
+                      const Positioned(
+                      top: 90,
+                          left: 50,
+                          child: Text("EMI",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)),
+                          const Positioned(
+                              top: 110,
+                              left: 30,
+                              child: Text("Calclator",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)),
+                              ],
+                            ),
+                      onTap: () {
+                      Navigator.pushNamed(context, "emi");
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top:50,left: 30),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        child: Container(
+                          height: 150,
+                          width: 140,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffE3D5E6),
+                            borderRadius: const BorderRadius.all(Radius.circular(10),),
+                              border: Border.all(color: const Color(0xffC796C5),width: 2)
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                          height: 70,
+                          width: 140,
+                          top: 16,
+                          child: Image(image: AssetImage("chart.png"),)
+                      ),
+                      const Positioned(
+                        top: 85,
+                          left: 36,
+                          child: Text("Compare",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)),
+                      const Positioned(
+                          top: 105,
+                          left: 55,
+                          child: Text("Loan",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)),
+                    ],
+                  ),
+                ),
+              ),
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
